999176226690665155a7dee42ab69ce190fe1c8c
jseba <jseba@DESKTOP-FA85BS6>
2023-01-01 21:37:03 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,131 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServicioUsuarios servicioUsuarios;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+            IServicioUsuarios servicioUsuarios)
+        {
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            return View(tiposCuentas);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
+            //Validamos que todo este correcto según el modelo
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            //Asignamos un usuario por defecto por el momento
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
+            //Verificamos que no exista un Tipo Cuenta con el mismo nombre
+            var yaExisteTipoCuenta =
+                await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+            if (yaExisteTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre),
+                    $"El nombre {tipoCuenta.Nombre} ya existe.");
+
+                return View(tipoCuenta);
+            }
+            //Si todo está correcto, guardamos el registro
+            await repositorioTiposCuentas.Crear(tipoCuenta);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(TipoCuenta tipoCuenta)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+            if (tipoCuentaExiste is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Actualizar(tipoCuenta);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarTipoCuenta(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Borrar(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerificarTipoCuenta(string nombre)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(nombre, usuarioId);
+            if (yaExisteTipoCuenta)
+            {
+                return Json($"El nombre {nombre} ya existe");
+            }
+
+            return Json(true);
+        }
+
+        
+    }
+}
---
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -0,0 +1,94 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using System.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioTiposCuentas
+    {
+        Task Actualizar(TipoCuenta tipoCuenta);
+        Task Borrar(int id);
+        Task Crear(TipoCuenta tipoCuenta);
+        Task<bool> Existe(string nombre, int usuarioId);
+        Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
+        Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
+    }
+    public class RepositorioTiposCuentas : IRepositorioTiposCuentas
+    {
+        private readonly string connectionString;
+        public RepositorioTiposCuentas(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task Crear(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"INSERT INTO TiposCuentas 
+                        (Nombre, UsuarioId, Orden)
+                        VALUES (@Nombre, @UsuarioId, 0);
+                        SELECT SCOPE_IDENTITY();";
+
+            var id = await connection.QuerySingleAsync<int>(qry, tipoCuenta);
+
+            tipoCuenta.Id = id;
+        }
+
+        public async Task<bool> Existe(string nombre, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"SELECT 1
+                        FROM TiposCuentas
+                        WHERE Nombre = @Nombre 
+                        AND UsuarioId = @UsuarioId;";
+
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(qry, new {nombre, usuarioId});
+
+            return existe == 1;
+        }
+
+        public async Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"SELECT Id, Nombre, Orden
+                        FROM TiposCuentas
+                        WHERE UsuarioId = @UsuarioId";
+            return await connection.QueryAsync<TipoCuenta>(qry, new { usuarioId });
+        }
+
+        public async Task Actualizar(TipoCuenta tipoCuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"UPDATE TiposCuentas
+                        SET Nombre = @Nombre
+                        WHERE Id = @Id";
+
+            await connection.ExecuteAsync(qry, tipoCuenta);
+        }
+
+        public async Task<TipoCuenta> ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"SELECT Id, Nombre, Orden
+                        FROM TiposCuentas
+                        WHERE Id = @Id AND UsuarioId = @UsuarioId";
+
+            return await connection.QueryFirstAsync<TipoCuenta>(qry, new {id, usuarioId});
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var qry = $@"DELETE TiposCuentas
+                        WHERE Id = @Id";
+
+            await connection.ExecuteAsync(qry, new { id });
+        }
+    }
+}
